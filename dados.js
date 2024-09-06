d86b5329196353d252b002577c7f006017137733
Italo Souza <95109439+RagnaReal@users.noreply.github.com>
2024-09-06 15:21:14 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+//Lista de objetos
+let dados = [
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Nascida em Guarulhos, a atleta conquistou o país com sua força, determinação e talento.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica"
+    },
+   
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, trazendo o skate ao pódio olímpico.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "skate sk8 fadinha fada"
+    },
+    
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Judoca brasileira, Beatriz Souza se destacou no cenário internacional, conquistando a primeira medalha de ouro do Brasil no judô nas Olimpíadas de Paris 2024. Sua força e técnica impecável a consagraram como uma das maiores atletas da modalidade.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "jodô judo judoca judoka ouro"
+    }
+]
